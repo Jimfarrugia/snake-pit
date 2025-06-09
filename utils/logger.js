6baaf8f0e1c6b263b23824b8ee25cc3c61586069
Jim Farrugia <the.jim.farrugia@gmail.com>
2025-06-10 05:40:19 +1000

    add logGameEvent helper for color-coded server logs

---
+++ utils/logger.js
@@ -0,0 +1,40 @@
+const chalk = require("chalk");
+
+const colorFns = [
+  chalk.magenta,
+  chalk.cyan,
+  chalk.yellow,
+  chalk.blue,
+  chalk.hex("#FF69B4"), // hotpink
+  chalk.hex("#FFA500"), // orange
+  chalk.hex("#00FF7F"), // spring green
+  chalk.hex("#1E90FF"), // dodger blue
+  chalk.red,
+  chalk.green,
+  chalk.whiteBright,
+  chalk.gray,
+];
+
+// This will map IDs to colors as they appear
+const idColorMap = new Map();
+let nextColorIndex = 0;
+
+// Get the color associated with an id
+function getColorFnForId(id) {
+  if (!idColorMap.has(id)) {
+    const colorFn = colorFns[nextColorIndex % colorFns.length];
+    idColorMap.set(id, colorFn);
+    nextColorIndex++;
+  }
+  return idColorMap.get(id);
+}
+
+// Log to the console using the color assigned to the id
+function logGameEvent(message, id) {
+  const colorFn = getColorFnForId(id);
+  console.log(colorFn(message));
+}
+
+module.exports = {
+  logGameEvent,
+};
