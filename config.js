3005e2f22e942f409c82427b74befb0537fb0cee
Jim Farrugia <the.jim.farrugia@gmail.com>
2025-06-11 03:00:50 +1000

    fix typo

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   initialSnakeLength: 5, // amount of segments that snakes begin with
   initialSpeed: 200, // initial snake movement interval duration in ms
   immunityDuration: 15000, // duration of immunity effect in ms
-  immunityRespawnTime: 20000, // time before immunity respawns in ms
+  immunityRespawnCooldown: 20000, // time before immunity respawns in ms
   speedBoostDuration: 5000, // duration of speed boost effect in ms
   speedBoostMultiplier: 0.75, // lower value = higher speed
   snakeMaxTargetSize: 3, // max amount of trailing snake segments that are vulnerable to attack
